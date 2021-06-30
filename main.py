ac4118a1653322b5d04bff87f361a0269139c5f1
ssl <77415460+sslprograms@users.noreply.github.com>
2021-06-30 12:42:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import requests, threading, random
+import requests, threading, random, string
 
 def check():
     try:
         users = ""
         for x in range(20):
-            letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'l', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8 ,9]
+            letters = string.ancii_letters + string.digits + string.ancii_letters
             users = users + str(random.choice(letters))
         print(users)
         username = requests.get(f'https://auth.roblox.com/v1/usernames/validate?birthday=2006-09-21T07:00:00.000Z&context=Signup&username={users}')
